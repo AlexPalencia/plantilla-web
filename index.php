312516734f72bf67cf21dc3f071d458f3af8f4c3
AlexPalencia <andres_palencia97@hotmail.com>
2020-07-15 15:30:28 -0500

    modelo inicial de la plantilla web hecha por Alex

---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <title>Index</title>
+    <script src="https://kit.fontawesome.com/c9117467ae.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous"> -->
+
+    <!-- boton whatsapp -->
+     <link rel="stylesheet" type="text/css" href="css/btnWhatsapp.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+  </head>
+  <body>
+	<div class="content">
+		
+		<?php
+			// navbar 
+			include('vistas/componentes/navbar.html');
+
+			// boton whatsapp
+			include('vistas/componentes/whatsapp.php');
+		 ?>
+
+	<!-- inicio del div miDiv backstrech -->
+	<div class="miDiv position-static">
+		<div class="text-center mb-3" style="margin-top: 100px;" >
+			<h1 class="display-4 d-inline-block p-2 box">
+				<strong> CORPORACIÓN CRERI </strong>
+			</h1>
+			<br>
+			<p class="d-inline-block p-2 box"><strong>Crear, crecer y creer en Rivera</strong></p>
+		</div>
+		<div class="text-center">
+			<button type="button" class="rounded-pill btn btn-lg btn-primary">Contáctanos</button>
+			<button type="button" class="rounded-pill btn btn-lg btn-light">Trabaja con nosotros</button> 
+		</div>
+		
+		<!-- Inicio de las cards -->
+		<div class="container">
+			<div class="card-deck justify-content-center my-5" >
+				<div class="card border-secondary  mb-3" style="max-width: 18rem;">
+				  <div class="card-header text-center text-primary">
+				  	<span>
+					 	<i class="fas fa-users fa-3x"></i>
+					</span>
+					
+				  </div>
+				  <div class="card-body  text-center">
+				    <h5 class="card-title">Surcolombianidad</h5>
+				    <p class="card-text">
+				    	En el marco del evento llamado: Semana de la surcolombianidad, que ha creado la universidad Surcolombiana; nos hemos vinculado para trasladar a nuestro municipio las actividades que dicho evento conllevan para dar a conocer las diferentes ofertas académicas que tiene esta alma máter.
+				    </p>
+				  </div>
+				</div>
+				<div class="card  border-secondary mb-3" style="max-width: 18rem;">
+				  <div class="card-header text-center text-primary">
+				  	<span>
+				  		<i class="far fa-handshake fa-3x"></i> 
+				  	</span>
+				  </div>
+				  <div class="card-body  text-center">
+				    <h5 class="card-title">Transporte universitario</h5>
+				    <p class="card-text">
+				    	La función que cumple la corporación con los jóvenes del municipio al facilitar el transporte para los estudiantes de educación superior del territorio hacia la ciudad de Neiva, creando más opciones de acceso a la educación superior por parte del compromiso solidario y fraternal que tiene la corporación para autogestionar este proceso.
+				    </p>
+				  </div>
+				</div>
+				<div class="card  border-secondary mb-3" style="max-width: 18rem;">
+				  <div class="card-header text-center text-primary">
+				  	<span>
+				  		<i class="fab fa-gratipay fa-3x"></i>
+				  	</span>
+				  </div>
+				  <div class="card-body  text-center">
+				    <h5 class="card-title">Otras actividades</h5>
+				    <p class="card-text">
+				    	Cada año al final de este, se hace una semana cultural de jóvenes para jóvenes del municpio, creando espacios para la cohesión social de grupos artísticos, literarios y de areas afines de las siete artes existentes.
+				    </p>
+				  </div>
+				</div>	
+			</div>
+		</div>
+		<!-- fin de las cards -->
+	
+	
+	<div class="content text-center mx-5">
+		<h1 class="display-4">Corporación CRERI Rivera - Huila.</h1>
+		<blockquote class="blockquote text-center">
+		  <p class="mb-0">
+		  	Bienvenido(a) al sitio web de la corporación crear, crecer y ceer en Rivera CRERI; donde encontrarás todo
+		    lo concerniente a nuestra entidad, la cual es lider a nivel municipal en planeación y ejecución de programas sociales,
+		    Te invitamos a explorar en los diferentes links que conforman este espacio digital, mas acerca de nosotros,
+ 			bienvenido(a).
+ 		  </p>
+		</blockquote>
+		
+	</div>
+	<!-- incio de cards de informacion -->
+	<div class="container">
+		<div class="card-deck justify-content-center my-5" >
+		  <div class="card card-shadow border-secondary mb-3" style="max-width: 18rem;">
+			  <div class="card-header text-primary text-center text-primary">
+			  	<span>
+			  		<i class="fas fa-landmark fa-3x"></i>
+			  	</span>
+			  </div>
+			  <div class="card-body text-center">
+			    <h5 class="card-title">Reseña Histórica</h5>
+			    <p class="card-text">
+			    	La corporación fue creada en el 2011 por un grupo de jóvenes universitarios que vieron la necesidad de organizarce para poder cear un sistema de tranporte de educación superior, puesto que en el municpio hay un gran número de jóvenes que quieren acceder a esta pero se presenta el factor limitante que es el transporte y el costo que este conlleva.
+				</p>
+			  </div>
+			  <div class="card-footer text-center">
+			  	<button type="button" class="btn btn-primary">¿Quieres saber más?</button>
+			  </div>
+			</div>
+			<div class="card card-shadow border-secondary mb-3" style="max-width: 18rem;">
+			  <div class="card-header text-center text-primary">
+			  	<span>
+			  		<i class="far fa-id-card fa-3x"></i>
+			  	</span>
+			  </div>
+			  <div class="card-body text-center">
+			    <h5 class="card-title">¿Quienes somos?</h5>
+			    <p class="card-text">
+			    	Somos la entidad sin animo de lucro líder en el departamento del Huila-Colombia , en planeacion y ejecución de programas sociales enfocados en la coordinación del trasporte universitario y creación de espcios sociales y culturales para la realización de encuentros juveniles en el municipio.
+			    </p>
+			  </div>
+			  <div class="card-footer text-center">
+			  	<button type="button" class="btn btn-primary">¿Quieres saber más?</button>
+			  </div>
+			</div>
+			<div class="card card-shadow border-secondary mb-3 animated zoomOutUp" style="max-width: 18rem;">
+			  <div class="card-header text-primary text-center">
+			  	<span>
+			  		<i class="fa fa-paper-plane fa-3x"></i>	
+			  	</span>
+			  </div>
+			  <div class="card-body text-center">
+			    <h5 class="card-title">Contáctenos</h5>
+			    <p class="card-text">
+			    	Contactanos Directamente A nuestro Correo Institucional	
+			    	<a href="#" class="font-weight-bold">torrentesoftware@gmail.com</a>
+			    </p>
+			  </div>
+			  <div class="card-footer text-center">
+			  	<button type="button" class="btn btn-primary">¿Quieres saber más?</button>
+			  </div>
+			</div>
+		</div>
+	</div>
+	<!-- fin de cards de informacion -->
+	<!-- inicio div contactenos -->
+	<div class="my-5" style="background-color: #00b0ff;">
+		<br>
+		<div class="mx-5" >
+		  <h1 class="text-dark mx-5">Contáctanos directamente a nuestras lineas de atencion.</h1>
+			  	<button type="button" class="btn btn-warning mx-5 mb-5">¡Da click aquí!</button>
+		  
+		</div>
+	</div>
+	<!-- fin div contactenos -->
+	<!-- proyectos -->
+	<div class="content text-center mx-5">
+		<h3><strong>Nuestros proyectos</strong></h3>
+		<blockquote class="blockquote text-center">
+			<h1 class="display-4">Un poco de nosotros</h1>
+		  <p class="mb-0">
+		  	A continuación veras un poco de nuestros grandes proyectos…
+ 		  </p>
+		</blockquote>
+		<div >
+			<img src="img/recurso1.jpg" class="rounded img-fluid mx-0 " alt="...">
+			<img src="img/recurso3.jpg" class="rounded img-fluid mx-5 " alt="...">
+			<img src="img/recurso2.png" class="rounded img-fluid mx-0 " alt="...">
+		</div>
+	</div>
+	<!-- fin de proyectos -->
+	<!-- inicio del formularo de contacto -->
+	<div class="jumbotron jumbotron-fluid" style="background: url('img/square6.png');">
+
+		<div class="container">
+		  <form class="text-center" style="background: white;">
+			<h2 class="pt-3">Contáctenos</h2>
+			<p>Envíanos un mensaje a nuestro correo electrónico y nos contactaremos contigo…</p>
+		    <div class="form-row justify-content-center">
+		      <div class="form-group col-md-4">
+		     	<label for="inputEmail4">Nombre completo</label>
+		      	<input type="text" class="form-control" id="inputEmail4">
+		      </div>
+		      <div class="form-group col-md-4">
+				      <label for="inputEmail4">Correo electrónico</label>
+				      <input type="email" class="form-control" id="inputPassword4">
+				   </div>
+		    </div>
+			  <div class="form-row">
+				
+			  </div>
+			  <div class="form-row justify-content-center">
+			  	  <div class="form-group col-md-4">
+				    <label for="inputAddress">Asunto</label>
+				    <input type="text" class="form-control" id="inputAddress">
+				  </div>
+				  <div class="form-group col-md-4">
+				    <label for="exampleFormControlTextarea1">Mensaje</label>
+				    <br>
+				    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+				  </div>
+			  </div>
+			  <button type="submit" class="btn btn-primary mb-3">Enviar</button>
+		  </form>
+			
+	    </div>
+	</div>
+	<!-- fin del formulario de contacto -->
+
+
+		<!-- Footer -->
+	<?php  
+		include('vistas/componentes/footer.html');
+	?>
+	<!-- Footer -->
+
+
+</div>
+<!-- fin del div del miDiv -->
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+<!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script> -->
+
+<!-- libreriaas del backstrech -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.backstretch.js"></script>
+<script>
+    $(".miDiv").backstretch([
+      "img/recurso2.png",
+      "img/recurso1.jpg",
+      "img/recurso3.jpg"
+      ], {
+        fade: 750,
+        duration: 4000,
+        preload: 1, //Use the lazy-loading functionality
+        start: 2 //Optional - now starts with "dome.jpg"
+    });
+
+ 
+</script>
+  </body>
+</html>
---
+++ vistas/componentes/whatsapp.php
@@ -0,0 +1,11 @@
+<?php 
+	$celular = '573128068315';
+	$mensaje = '¡Hola! Quisiera saber más acerca de su producto y mirar más diseños de plantillas web.';
+?>  
+     
+<a href="https://api.whatsapp.com/send?phone=<?php echo $celular ?>&text=<?php echo $mensaje ?>" class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+  
+  
+
